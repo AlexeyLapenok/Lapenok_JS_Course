322a37b372c9e54e5a7c0eefa0c40bb73016ae07
LapenokAlexey <aleks.lapenok@gmail.com>
2021-04-24 20:41:25 +0300

    ht2todo refactor: some refactoring

--- tasks/task2todo/src/js/components/search.js
+++ tasks/task2todo/src/js/components/search.js
@@ -1,10 +1,8 @@
-// Search by letter of task(doesn't work with complited tasks)
+// Search by letter of task
 const search = document.querySelector('.header_input');
 const list = document.querySelector('.list-container_task-list');
 search.addEventListener('input', (evt) => {
   list.childNodes.forEach((node) => {
-    const text = node.querySelectorAll('.is-done');
-    console.log(text);
     if (node.querySelector('p').innerHTML.includes(evt.target.value)) {
       // eslint-disable-next-line no-param-reassign
       node.style.display = 'flex';
